572f15e93fc1a839642288a1406332b85e0d9886
divyesh1501 <divyesh429.dp@gmail.com>
2024-03-05 20:23:03 +0530

    update

--- React JS/Task/Product-Task/src/UI/Pages/Product/Product.jsx
+++ React JS/Task/Product-Task/src/UI/Pages/Product/Product.jsx
@@ -1,40 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Button, Table } from 'reactstrap';
 import "./index.css"
 import ProductModal from '../../Components/Modal/ProductModal';
-import { Pencil, Trash2 } from "lucide-react";
+import TableData from '../TableData';
+import { toast } from 'react-toastify';
 
+const initialData = {
+  title: "",
+  description: "",
+  brand: "",
+  category: [],
+  price: "",
+  gender: "",
+  discountPercentage: "0",
+  color: [],
+  size: [],
+
+}
 
 export default function Product() {
-  const [product, setProduct] = useState([]);
   const [modal, setModal] = useState(false);
-  const [selectedProduct, setSelectedProduct] = useState(null)
-  const [data, setData] = useState(false)
+  let [fetchFlag, setFetchFlag] = useState(true)
+  let [data, setData] = useState([])
+  let [productData, setProductData] = useState(initialData)
 
   const toggle = () => setModal(!modal);
-  const showElipsis =()=>{
-    setData(!data)
-  }
-
-  const updateData = (index) => {
-    setSelectedProduct(product[index])
-    toggle()
-  }
-
-  const deletData = (id) => {
-    axios({
-      method:"delete",
-      url:`http://localhost:9999/product/delete/${id}`
-    })
-    .then((res)=>{
-      alert("Product Removed ...!!")
-      showElipsis()
-    })
-    .catch((err)=>{
-      alert("error")
-    });
-  };
+  const reFetchData = () => setFetchFlag(!fetchFlag)
 
   useEffect(() => {
     axios({
@@ -42,65 +33,76 @@ export default function Product() {
       url: 'http://localhost:9999/product/getAll',
     })
       .then((res) => {
-        console.log("------->", res.data);
-        setProduct(res?.data?.data);
+        setData(res?.data?.data);
       })
       .catch((err) => {
-        console.log("-------->", err);
+        toast.error(err.response)
       });
-  }, [data]);
+  }, [fetchFlag]);
+
+
+  const submitHandler = (data) => {
+    axios({
+      method: "post",
+      url: `http://localhost:9999/product/create`,
+      data: data,
+    }).then((res) => {
+      toast.success("Product Added Succesfully");
+      setAddProduct(initialData);
+      reFetchData()
+      toggle();
+    })
+      .catch((err) => {
+        toast.error(err.response)
+      });
+    reFetchData()
+    toggle()
+  }
+
+  const updateHandler = (newData) => {
+    axios({
+      method: "put",
+      url: `http://localhost:9999/product/update/${newData?._id}`,
+      data: newData,
+    })
+      .then((res) => {
+        toast.success("Product Updated Succesfully");
+        setAddProduct(initialData);
+        reFetchData()
+        toggle();
+      })
+      .catch((err) => {
+        toast.error(err.response);
+      });
+    reFetchData()
+    toggle();
+  }
+
+  const deleteHandler = (id) => {
+    axios({
+      method: "delete",
+      url: `http://localhost:9999/product/delete/${id}`
+    })
+      .then((res) => {
+        reFetchData()
+        toast.success("Product Removed Succesfully");
+        
+      })
+      .catch((err) => {
+        toast.error(err.response);
+
+      });
+  };
+
+  const editHandler = (data) => {
+    setProductData(data)
+    toggle()
+  }
 
   return (
     <>
-      <ProductModal modal={modal} toggle={toggle} seledctedProduct={selectedProduct} showElipsis={showElipsis} />
-      <div>
-        <div className='d-flex justify-content-end pb-3 pe-5'>
-          <Button style={{ backgroundColor: "#42a1e994", color: "black", border: "none" }} onClick={toggle}>Add Product</Button>
-        </div>
-        {product.length === 0 ? <span style={{fontWeight:"bold", fontSize:"30px", display:"flex", width:"100%", justifyContent:"center", marginBottom:"25px"}}>Data Not Found ..!!</span>: (<div div className='p-5'>
-
-          <Table dark striped size='sm' style={{ width: "100%", textAlign: "center" }}>
-            <thead>
-              <tr>
-                <th>Sr.No</th>
-                <th>Title</th>
-                <th>Description</th>
-                <th>Brand</th>
-                <th>Gender</th>
-                <th>Price</th>
-                <th>Discount</th>
-                <th>Category</th>
-                <th style={{ width: "150px" }}>Color</th>
-                <th>Size</th>
-                <th>Image</th>
-                <th>Edit</th>
-                <th>Remove</th>
-              </tr>
-            </thead>
-            <tbody >
-              {product.map((e, i) => (
-                <tr key={i}>
-                  <th scope="row">{i + 1}</th>
-                  <td>{e.title}</td>
-                  <td>{e.description}</td>
-                  <td>{e.brand}</td>
-                  <td>{e.gender}</td>
-                  <td>{e.price}</td>
-                  <td >{e.discountPercentage}</td>
-                  <td>{e.category}</td>
-                  <td>{e.color.join(' ,')}</td>
-                  <td>{e.size.join(',')}</td>
-                  <td><img style={{width:"100%", aspectRatio:"3/2"}} src={e.thumbnail}alt="" /></td>
-                  <th><Pencil onClick={() => updateData(i)} color="yellow" role="button" /></th>
-                  <th><Trash2 onClick={() => deletData(e._id)} color="red" role="button" /></th>
-
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        </div>)
-        }
-      </div >
+      <ProductModal productData={productData} modal={modal} toggle={toggle} updateHandler={updateHandler} submitHandler={submitHandler} />
+      <TableData toggle={toggle} reFetchData={reFetchData} productData={data} editHandler={editHandler} deleteHandler={deleteHandler} />
     </>
   );
 }
---
+++ React JS/Task/Product-Task/src/UI/Pages/TableData.jsx
@@ -0,0 +1,66 @@
+import { Pencil, Trash2 } from 'lucide-react'
+import React, { useEffect, useState } from 'react'
+import { Button, Table } from 'reactstrap'
+
+export default function TableData({ toggle, productData, editHandler, deleteHandler }) {
+
+    const [tableData, setTableData] = useState(productData)
+
+    useEffect(() => {
+        setTableData(productData)
+    }, [productData])
+
+    return (
+        <>
+            <div>
+                <div className='d-flex justify-content-end pb-3 pe-5'>
+                    <Button style={{ backgroundColor: "#42a1e994", color: "black", border: "none" }} onClick={toggle}>Add Product</Button>
+                </div>
+                {tableData.length === 0 ? <span style={{ fontWeight: "bold", fontSize: "30px", display: "flex", width: "100%", justifyContent: "center", marginBottom: "25px" }}>Data Not Found ..!!</span> : (<div className='p-5'>
+
+                    <Table dark striped size='sm' style={{ width: "100%", textAlign: "center" }}>
+                        <thead>
+                            <tr>
+                                <th>Sr.No</th>
+                                <th>Title</th>
+                                <th>Description</th>
+                                <th>Brand</th>
+                                <th>Gender</th>
+                                <th>Price</th>
+                                <th>Discount</th>
+                                <th>Category</th>
+                                <th style={{ width: "150px" }}>Color</th>
+                                <th>Size</th>
+                                <th>Image</th>
+                                <th>Edit</th>
+                                <th>Remove</th>
+                            </tr>
+                        </thead>
+                        <tbody >
+                            {tableData.map((e, i) => (
+                                <tr key={i}>
+                                    <th scope="row">{i + 1}</th>
+                                    <td>{e.title}</td>
+                                    <td>{e.description}</td>
+                                    <td>{e.brand}</td>
+                                    <td>{e.gender}</td>
+                                    <td>{e.price}</td>
+                                    <td >{e.discountPercentage}</td>
+                                    <td>{e.category}</td>
+                                    <td>{e.color.join(' ,')}</td>
+                                    <td>{e.size.join(',')}</td>
+                                    <td><img style={{ width: "100%", aspectRatio: "3/2" }} src={e.thumbnail} alt="" /></td>
+                                    <th><Pencil onClick={() => editHandler(e)} color="yellow" role="button" /></th>
+                                    <th><Trash2 onClick={() => deleteHandler(e._id)} color="red" role="button" /></th>
+
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                </div>
+                )}
+            </div>
+        </>
+    )
+}
+
